6c1f9c08d4c5015a2cc37a875daaddb0ed1631c6
Alex Liu <liuzhendell@gmail.com>
2020-12-18 10:15:31 -0700

    add datepicker

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -8,11 +8,14 @@ import { NgForm } from '@angular/forms';
 })
 export class SignupComponent implements OnInit
 {
+  maxDate;
 
   constructor() { }
 
   ngOnInit(): void
   {
+    this.maxDate = new Date();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
   }
 
   onSubmit(form: NgForm)
